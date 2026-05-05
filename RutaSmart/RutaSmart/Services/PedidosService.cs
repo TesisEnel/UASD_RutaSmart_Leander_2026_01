6aab43c5b45aa116aafa16a77ec731405b75bd23
Leander-Valdez <leandervaldezreyes@gmail.com>
2026-05-05 10:32:57 -0400

    Implementar pedido Service

--- RutaSmart/RutaSmart/Services/PedidosService.cs
+++ RutaSmart/RutaSmart/Services/PedidosService.cs
@@ -3,10 +3,30 @@ using RutaSmart.Data.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Aplicada1.Core;
+using System.Linq.Expressions;
 
 namespace RutaSmart.UI.Services;
 
 public class PedidosService(RutaSmartContext context) : IService<Pedido, int>
 {
+    public Task<Pedido?> Buscar(int id)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task<bool> Eliminar(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Pedido>> GetList(Expression<Func<Pedido, bool>> criterio)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Guardar(Pedido entidad)
+    {
+        throw new NotImplementedException();
+    }
 }
