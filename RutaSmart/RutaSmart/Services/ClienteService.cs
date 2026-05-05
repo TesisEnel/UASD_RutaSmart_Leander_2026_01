7f8f871493fb577e47986196bd184fd691636cb0
Leander-Valdez <leandervaldezreyes@gmail.com>
2026-05-05 09:05:49 -0400

    Implementacion de service y test

---
+++ RutaSmart/RutaSmart/Services/ClienteService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Aplicada1.Core;
+using RutaSmart.Data.Data;
+using RutaSmart.Data.Models;
+
+namespace RutaSmart.UI.Services;
+
+
+public class ClienteService(RutaSmartContext context) : IService<Cliente, int>
+{
+    public Task<Cliente?> Buscar(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Eliminar(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Cliente>> GetList(Expression<Func<Cliente, bool>> criterio)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Guardar(Cliente entidad)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> Insertar(Cliente cliente)
+    {
+        context.Clientes.Add(cliente);
+        return (await context.SaveChangesAsync()) > 0;
+    }
+
+    public async Task<bool> Modificar(Cliente cliente)
+    {
+        context.Update(cliente);
+        return (await context.SaveChangesAsync()) > 0;
+    }
+}
---
+++ RutaSmart/RutaSmart/Services/PedidosService.cs
@@ -0,0 +1,12 @@
+﻿using RutaSmart.Data.Data;
+using RutaSmart.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RutaSmart.UI.Services;
+
+public class PedidosService(RutaSmartContext context) : IService<Pedido, int>
+{
+
+}
