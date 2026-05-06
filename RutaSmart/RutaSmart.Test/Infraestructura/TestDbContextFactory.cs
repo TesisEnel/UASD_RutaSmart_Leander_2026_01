a3000b65e2e615a704bafa16be17f4184355981d
Leander-Valdez <leandervaldezreyes@gmail.com>
2026-05-05 22:13:36 -0400

    Implementacion de PedidosService

--- RutaSmart/RutaSmart.Test/Infraestructura/TestDbContextFactory.cs
+++ RutaSmart/RutaSmart.Test/Infraestructura/TestDbContextFactory.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace RutaSmart.Test.Infraestructura
 {
     public class TestDbContextFactory
--- RutaSmart/RutaSmart/Services/PedidosService.cs
+++ RutaSmart/RutaSmart/Services/PedidosService.cs
@@ -5,28 +5,53 @@ using System.Collections.Generic;
 using System.Text;
 using Aplicada1.Core;
 using System.Linq.Expressions;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace RutaSmart.UI.Services;
 
 public class PedidosService(RutaSmartContext context) : IService<Pedido, int>
 {
-    public Task<Pedido?> Buscar(int id)
+    public async Task<Pedido?> Buscar(int id)
     {
-        throw new NotImplementedException();
+        return await context.Pedidos.FindAsync(id);
     }
 
-    public Task<bool> Eliminar(int id)
+    public async Task<bool> Eliminar(int id)
     {
-        throw new NotImplementedException();
+        var pedido = await context.Pedidos.FindAsync(id);
+
+        if (pedido == null)
+            return false;
+
+        context.Pedidos.Remove(pedido);
+        return (await context.SaveChangesAsync()) > 0;
     }
 
-    public Task<List<Pedido>> GetList(Expression<Func<Pedido, bool>> criterio)
+    public async Task<List<Pedido>> GetList(Expression<Func<Pedido, bool>> criterio)
     {
-        throw new NotImplementedException();
+        return await context.Pedidos
+            .Where(criterio)
+            .ToListAsync();
     }
 
-    public Task<bool> Guardar(Pedido entidad)
+    public async Task<bool> Guardar(Pedido entidad)
     {
-        throw new NotImplementedException();
+        if (entidad.PedidoId == 0)
+            return await Insertar(entidad);
+        else
+            return await Modificar(entidad);
+    }
+
+    public async Task<bool> Insertar(Pedido pedido)
+    {
+        context.Pedidos.Add(pedido);
+        return (await context.SaveChangesAsync()) > 0;
+    }
+
+    public async Task<bool> Modificar(Pedido pedido)
+    {
+        context.Update(pedido);
+        return (await context.SaveChangesAsync()) > 0;
     }
 }
